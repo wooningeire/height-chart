0b5157417edcdd03e1bcebf977898b8294471442
wooningeire <22846982+wooningeire@users.noreply.github.com>
2025-08-10 04:37:26 -0400

    chore: add vitest and playwright

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,9 @@
-import { pgTable, serial, text, integer, uuid, varchar, vector, real, jsonb } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer, uuid, varchar, vector, real, jsonb, bigint, foreignKey } from 'drizzle-orm/pg-core';
+
+export const userTable = pgTable("user", {
+	discordId: bigint({mode: "number"}).notNull(),
+})
+	.enableRLS();
 
 export const characterTable = pgTable("character", {
 	id: serial().primaryKey(),
@@ -8,4 +13,21 @@ export const characterTable = pgTable("character", {
 	offsetPos: real().array().notNull(),
 	offsetScale: real().array().notNull(),
 })
+	.enableRLS();
+
+
+export const collectionTable = pgTable("collection", {
+	id: serial().primaryKey(),
+	name: varchar({length: 256}).notNull(),
+})
+	.enableRLS();
+
+
+export const characterBelongsToCollectionTable = pgTable("character_belongs_to_collection", {
+	characterId: integer().notNull(),
+	collectionId: integer().notNull(),
+}, table => [
+	foreignKey({columns: [table.characterId], foreignColumns: [characterTable.id]}),
+	foreignKey({columns: [table.collectionId], foreignColumns: [collectionTable.id]}),
+])
 	.enableRLS();
